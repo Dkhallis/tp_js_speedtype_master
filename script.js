e3380b3fe85761972a1f73f4421022ebc844fcc3
Oscar <saguaebmax@gmail.com>
2026-02-25 16:26:05 +0100

    Lance un chrono

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 /* 1/ Charge un paragraphe depuis une API. */
+
 fetch('https://speedtype.api.pierre-jehan.com/')
 
 const btnStart = document.querySelector("#btn-start"); 
@@ -25,28 +26,29 @@ btnStart.addEventListener("click", function(){
     .catch(error => {
         console.error("Erreur : ", error); 
     });
+    startTimer(duree); 
 
 }); 
 
-
-
-
-
-
-
-
-
-
 /* 2/ Lance un chrono. */
+let timeLeft; 
+let timer; 
 
-
-
-
-
-
-
-
-
+function startTimer(duree) {
+    timeleft = duree; 
+    timer = setInterval(function(){
+        timeLeft--; 
+        document.querySelector("#time-left").textContent = timeLeft; 
+        if (timeLeft <= 0) {
+            clearInterval(timer); 
+            endGame(); 
+        }
+    }, 1000); 
+}
+function endGame() {
+    document.querySelector("#text-input").disabled = true; 
+    alert("Temps écoulé !"); 
+}
 
 
 /* 3/ Compare ce que tape, l'utilisateur avec le texte affiché.*/
